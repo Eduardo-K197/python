6b12752eee5bb8cde4e53ebeb4a608b251031bab
Eduardo <carloseduardobezerradasilva2@gmail.com>
2023-04-19 16:18:37 -0300

    up adivinhacao_2.1

---
+++ algoritimos/interpolacao_string.py
@@ -0,0 +1,6 @@
+nome = 'carlos Eduardo {1} da {0}'.format('Bezerra', 'silva')
+print(nome)
+
+valorReal = 49.65
+
+print('R$ {:07.2f}'.format(valorReal))
--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -5,21 +5,45 @@ print('Bem vindo ao jogo de adivinhação!')
 print('########################################')
 
 tentativa = 3
+pontos = 100
+nivel = int(input('''Em qual nível você deseja jogar?
+          [0]Básico
+          [1]Médio
+          [2]Difícil
+
+    Digite sua escolha: '''))
+
+match nivel:
+    case 0:
+        tentativa = 8
+    case 1:
+        tentativa = 5
+    case 2:
+        tentativa = tentativa
+
 numbSecret = random.randint(1,10)
 
 while tentativa > 0:
 
-    print('Você tem {} de três tentativas!'.format(tentativa))
-
-    tentativa -= 1
+    print('Você tem {} tentativas e {} pontos!'.format(tentativa, pontos))
 
     numeroUsuario = input('Digite um número: ')
 
+    #Validações
+
     if not numeroUsuario.isdigit():
         print('só aceitamos números')
-        break
+        continue
+
+    if float(numeroUsuario) < 0:
+        print('E números positivos')
+        continue
 
     numeroUsuario = int(numeroUsuario)
+
+    tentativa -= 1
+
+    #pontuação e resultado
 
     acertou = numbSecret == numeroUsuario
 
@@ -27,10 +51,13 @@ while tentativa > 0:
         print('O número correto era {} Você errou!'.format(numbSecret))
         break
     if acertou:
-        print('Você acertou!')
+        print(f'Você acertou! e com {pontos} pontos parabens!')
         break
     else:
+        pontos -= 10
         if numeroUsuario > numbSecret:
-            print('Você errou ! o número é menor')
+            print('Você errou ! o número é menor perdeu 10 pontos')
+            print('########################################')
         else:
-            print('Você errou ! o número é maior')
+            print('Você errou ! o número é maior perdeu 10 pontos')
+            print('########################################')
